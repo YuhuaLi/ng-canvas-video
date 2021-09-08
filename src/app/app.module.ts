b28c9f6563391f0a5db8fa216edb4c79d0c52a5c
liyuhua <liyuhua@example.com>
2021-09-08 14:49:11 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgCanvasVideoModule } from 'projects/ng-canvas-video/src/lib/ng-canvas-video.module';
-import { NgTimeSliderModule } from 'projects/ng-time-slider/src/public-api';
-// import { NgCanvasVideoModule } from 'ng-canvas-video';
+// import { NgCanvasVideoModule } from 'projects/ng-canvas-video/src/lib/ng-canvas-video.module';
+// import { NgTimeSliderModule } from 'projects/ng-time-slider/src/public-api';
+import { NgCanvasVideoModule } from 'ng-canvas-video';
+import { NgTimeSliderModule } from 'ng-time-slider';
 
 @NgModule({
   declarations: [AppComponent],
